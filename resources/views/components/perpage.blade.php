1d4180c43ec17513ba1d4e78c8921b0387446f4b
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2024-05-05 06:41:12 +0100

    mobile responsive fixed

--- resources/views/components/perpage.blade.php
+++ resources/views/components/perpage.blade.php
@@ -1,54 +1,91 @@
-<div class="datatable-top">
-    <div class="datatable-dropdown">
-        <label>
-            <select class="datatable-selector" {{ $wirePageAction }}>
-                @foreach ($perPages as $perPage)
-                <option value="{{ $perPage }}">{{ $perPage }}</option>
-                @endforeach
-            </select>
-        </label>
+<div class="datatable">
+    <div class="datatable-top">
+        <div class="datatable-dropdown">
+            <label>
+                <select class="datatable-selector" {{ $wirePageAction }}>
+                    @foreach ($perPages as $perPage)
+                    <option value="{{ $perPage }}">{{ $perPage }}</option>
+                    @endforeach
+                </select>
+
+            </label>
+        </div>
+        @isset($wireSearchAction)
+        <div class="datatable-search">
+            <input class="datatable-input" type="text" placeholder="Search..." {{ $wireSearchAction }}>
+        </div>
+        @endisset
     </div>
-    @isset($wireSearchAction)
-    <div class="datatable-search">
-        <input class="datatable-input" placeholder="Search..." {{ $wireSearchAction }} type="search" title="Search...">
-    </div>
-    @endisset
 </div>
 
 <style>
-    .datatable-top,
-    .datatable-bottom {
-        padding: 8px 10px;
+    .datatable {
+        padding: 2px;
     }
 
-    .datatable-top::after,
-    .datatable-bottom::after {
-        clear: both;
-        content: " ";
-        display: table;
+    .datatable-top,
+    .datatable-bottom {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+        margin-bottom: 10px;
     }
 
     .datatable-selector,
     .datatable-input {
         padding: 6px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
     }
 
-    .datatable-top {
-        display: flex;
-        justify-content: space-between;
-        max-width: 500px;
+    .datatable-selector {
+        margin-right: 10px;
     }
 
-    @media only screen and (max-width: 900px) {
-        .datatable-top {
-            flex-wrap: wrap;
-            justify-content: center;
+    .datatable-input {
+        flex: 1;
+        margin-top: 10px;
+    }
+
+    .pagination {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        text-align: center;
+    }
+
+    .pagination li {
+        display: inline;
+        margin-right: 5px;
+    }
+
+    .pagination li a {
+        text-decoration: none;
+        padding: 6px 10px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        color: #333;
+    }
+
+    .pagination li a:hover {
+        background-color: #f0f0f0;
+    }
+
+    @media only screen and (max-width: 600px) {
+
+        .datatable-selector,
+        .datatable-input {
+            width: 100%;
+            margin-right: 0;
         }
-    }
 
-    @media only screen and (max-width: 375px) {
+        .datatable-input {
+            margin-top: 0;
+        }
+
         .datatable-top {
-            max-width: 100%;
+            justify-content: center;
         }
     }
 </style>
