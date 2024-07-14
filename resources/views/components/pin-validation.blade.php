af8ffb940b50f52fea32da9ba4f237ad28234e8c
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2024-07-14 21:46:09 +0100

    education api integrated

--- resources/views/components/pin-validation.blade.php
+++ resources/views/components/pin-validation.blade.php
@@ -13,33 +13,33 @@
         <form wire:submit="validatePin">
             <div class="pin-request">
                 <div class="pin-display">
-                    <input type="password" class="form-control @error('pin') is-invalid @enderror" wire:model="pin" id="pin"  readonly />
+                    <input type="password" class="form-control @error('pin') is-invalid @enderror" id="pin-input"  maxlength="4" readonly />
                     @error('pin')
                         <span style="font-size: 15px" class="text-danger mt-0 pt-0">{{ $message }}</span>
                     @enderror
                 </div>
                 <div class="pin-buttons">
                     <div class="">
-                        @foreach (range(1, 3) as $digit)
-                            <button type="button" wire:click="addDigit({{ $digit }})">{{ $digit }}</button>
-                        @endforeach
+                        <button type="button" onclick="addPin(1)">1</button>
+                        <button type="button" onclick="addPin(2)">2</button>
+                        <button type="button" onclick="addPin(3)">3</button>
                     </div>
                     <div class="">
-                        @foreach (range(4, 6) as $digit)
-                            <button type="button" wire:click="addDigit({{ $digit }})">{{ $digit }}</button>
-                        @endforeach
+                        <button type="button" onclick="addPin(4)">4</button>
+                        <button type="button" onclick="addPin(5)">5</button>
+                        <button type="button" onclick="addPin(6)">6</button>
                     </div>
                     <div class="">
-                        @foreach (range(7, 9) as $digit)
-                            <button type="button" wire:click="addDigit({{ $digit }})">{{ $digit }}</button>
-                        @endforeach
+                        <button type="button" onclick="addPin(7)">7</button>
+                        <button type="button" onclick="addPin(8)">8</button>
+                        <button type="button" onclick="addPin(9)">9</button>
                     </div>
                     <div class="">
-                        <button type="button" class="btn-danger" wire:click="deletePin">
+                        <button type="button" class="btn-danger" onclick="deletePin()">
                             <i class="fa-solid fa-eraser"></i>
                         </button>
-                        <button type="button" wire:click="addDigit(0)">0</button>
-                        <button type="submit" class="btn-success">
+                        <button type="button" onclick="addPin(0)">0</button>
+                        <button type="button" onclick="submitPin()" class="btn-success">
                             <i class="fa-solid fa-paper-plane"></i>
                         </button>
                     </div>
@@ -58,7 +58,7 @@
                         <p style="font-size: 25px" class="m-0 p-0"><i class="fa fa-spinner fa-spin"></i></p>
                         <p class="m-0 p-0">Processing...</p>
                     </div>
-                    <button type="button" wire:loading.remove wire:target='submit' class="btn btn-danger btn-sm" wire:click="closeModal">No</button>
+                    <button type="button" wire:loading.remove wire:target='submit' class="btn btn-danger btn-sm" id="closeModal" wire:click="closeModal">No</button>
                     <button type="submit" wire:loading.remove wire:target='submit' class="btn bg-basic text-light btn-sm" id="confirmYes">Yes</button>
                 </div>
             </div>
@@ -66,3 +66,46 @@
         @endif
     </div>
 </div>
+
+@push('scripts')
+<script>
+    let pin = '';
+
+    function addPin(num) {
+        if (pin.length < 4) {
+            pin += num;
+            document.getElementById('pin-input').value = pin;
+            @this.set('pin', pin);
+        }
+    }
+
+    function deletePin() {
+        pin = pin.slice(0, -1);
+        document.getElementById('pin-input').value = pin;
+        @this.set('pin', pin);
+    }
+
+    function clearPin() {
+        pin = '';
+        document.getElementById('pin-input').value = pin;
+        @this.set('pin', pin);
+    }
+
+    function submitPin() {
+        @this.call('validatePin').then(response => {
+        }).catch(error => console.error('Error:', error));
+    }
+
+    const closeOffcanvas = document.getElementById('closeOffcanvas');
+    closeOffcanvas.addEventListener('click', function () {
+        pin = "";
+        document.getElementById('pin-input').value = pin;
+    });
+
+    const closeModal = document.getElementById('closeModal');
+    closeModal.addEventListener('click', function () {
+        pin = "";
+        document.getElementById('pin-input').value = pin;
+    });
+</script>
+@endpush
