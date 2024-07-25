507ea1417041fde99b563215ee31e3dbf2a11b69
ikechukwu-unegbu <iunegbu94@yahoo.com>
2024-07-25 10:49:02 +0100

    fixed forgot password issue

--- app/Http/Controllers/V1/API/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/V1/API/Auth/ForgotPasswordController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\V1\API\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Session;
+// use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 
 class ForgotPasswordController extends Controller
@@ -19,19 +21,33 @@ class ForgotPasswordController extends Controller
             return response()->json(['error' => $validator->errors()], 400);
         }
 
+       
         $status = Password::sendResetLink(
             $request->only('email')
         );
+        
+        
+        // return $status === Password::RESET_LINK_SENT
+        //             ? response()->json([
+        //                 'message' => __($status),
+        //                 'status'=>'success'
+        //             ], 200)
+        //             : response()->json([
+        //                 'error' => __($status),
+        //                 'status'=>'failed'
+        //             ], 500);
 
-        return $status === Password::RESET_LINK_SENT
-                    ? response()->json([
-                        'message' => __($status),
-                        'status'=>'success'
-                    ], 200)
-                    : response()->json([
-                        'error' => __($status),
-                        'status'=>'failed'
-                    ], 500);
+        if ($status === Password::RESET_LINK_SENT) {
+            Session::flash('done', "Password reset link sent to your email.");
+            return redirect()->to(url()->previous() . '?success=1');
+
+        } else {
+
+            Session::flash('error', "Unable to send password reset link.");
+            return redirect()->back();
+        }
+
+        
     }
 
 
--- resources/views/auth/new-forgot-password.blade.php
+++ resources/views/auth/new-forgot-password.blade.php
@@ -10,20 +10,30 @@
     
     </div>
     <div class="right_auth">
-        <form class="form_auth">
+        <form class="form_auth" method="POST" action="{{route('password.email')}}">
+           @csrf 
             <div class="mb-3 fs-3 fw-semibold ">
                 <div class="auth_header">
-                    Login and Flex
+                    Enter Your Email For Password Reset Link
                 </div>
             <div>
+            @if (session('status'))
+                <div class="text-green-700 text-sm bg-green-100 border border-green-300 rounded p-4 mb-4">
+                    {{session('status')}}
+                </div>
+            @endif 
             @include("components.error_message")
+    
+            
+
+            
             <div class="form-floating mb-3">
                 <input type="text" name="email" class="form-control" id="floatingInput" placeholder="">
                 <label for="floatingInput">Email</label>
             </div>
           
             <div>
-                <button type="button" class="btn btn-warning">Email Password Reset Link</button>
+                <button type="submit" class="btn vastel-bg">Email Password Reset Link</button>
             </div>
         </form>
     
