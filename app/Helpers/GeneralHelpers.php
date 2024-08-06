2f7765c388fbb47423c43fb188befd7c9b9c61b5
ikechukwu-unegbu <iunegbu94@yahoo.com>
2024-08-06 18:20:06 +0100

    api for upgrade requst added

---
+++ app/Helpers/GeneralHelpers.php
@@ -0,0 +1,25 @@
+<?php 
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class GeneralHelpers{
+        /**
+     * Generate a unique UUID for the given table.
+     *
+     * @param string $table
+     * @return string
+     */
+    public static function generateUniqueUuid(string $table): string
+    {
+        $uuid = Str::uuid();
+
+        while (DB::table($table)->where('uuid', $uuid)->exists()) {
+            $uuid = Str::uuid();
+        }
+
+        return $uuid;
+    }
+
+}
---
+++ app/Services/UpgradeService.php
@@ -0,0 +1,22 @@
+<?php 
+namespace App\Services;
+
+use App\Helpers\GeneralHelpers;
+use App\Models\Utility\UpgradeRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UpgradeService{
+
+    public function __construct()
+    {
+        
+    }
+
+    public function requestUpgrate():void
+    {
+        $requestModel = new UpgradeRequest();
+        $requestModel->uuid = GeneralHelpers::generateUniqueUuid('upgrade_requests');
+        $requestModel->user_id = Auth::user()->id;
+        $requestModel->save();
+    }
+}
