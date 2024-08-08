90cfe983c80ac579fa5ce2d7371f03d23b37fd12
ikechukwu-unegbu <iunegbu94@yahoo.com>
2024-08-08 01:02:02 +0100

    M

--- app/Http/Controllers/V1/API/FileUploadController.php
+++ app/Http/Controllers/V1/API/FileUploadController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\V1\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Api\FileUpdateRequest;
 use App\Http\Requests\V1\API\FileUploadRequest;
+use App\Models\User;
 use App\Services\Uploads\ImageService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -19,17 +21,15 @@ class FileUploadController extends Controller
     public function store(FileUploadRequest $request)
     {
        
-        $fileUrl = $this->imageService->storeAvatar($request);
+        $user = User::find(Auth::user()->id);
+        if($user->image == null){
+            $fileUrl = $this->imageService->storeAvatar($request);
+        }else{
+            $fileUrl = $this->imageService->updateAvatar($request);
+        }
+        
         return response()->json([
             "file"=>$fileUrl
-        ]);
-    }
-
-    public function update(FileUpdateRequest $request)
-    {
-        $fileUrl = $this->imageService->updateAvatar($request);
-        return response()->json([
-            'file'=>$fileUrl
         ]);
     }
 
