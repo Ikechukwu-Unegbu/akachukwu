a980806706d99a04c237aeb0c8b0cdb7fbb1e215
ikechukwu-unegbu <iunegbu94@yahoo.com>
2024-08-15 10:12:13 +0100

    M

--- resources/views/livewire/admin/crd-dbt/create.blade.php
+++ resources/views/livewire/admin/crd-dbt/create.blade.php
@@ -47,10 +47,10 @@
                 <label for="dropdownSelect" class="form-label">Select</label>
                 <select name="action" class="form-select" id="dropdownSelect">
                     <option selected>Select one...</option>
-                    @if(auth()->user()->can('debit'))
+                    @if(auth()->user()->can('can debit'))
                     <option value="debit">Debit</option>
                     @endif 
-                    @if(auth()->user()->can('credit'))
+                    @if(auth()->user()->can('can top-up'))
                     <option value="credit">Credit</option>
                     @endif 
                 </select>
