c92cab3d0feb49f3144a3dfc11af718b27be1d17
ikechukwu-unegbu <iunegbu94@yahoo.com>
2024-09-04 08:27:19 +0100

    yesterday work

--- resources/views/layouts/new-ui.blade.php
+++ resources/views/layouts/new-ui.blade.php
@@ -9,6 +9,16 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        .floating-label-input:focus + label,
+        .floating-label-input:not(:placeholder-shown) + label {
+            transform: translateY(-1.5rem);
+            font-size: 0.75rem;
+            color: #3b82f6;
+            display: none;
+        }
+    </style>
 </head>
 
 <body class="bg-white text-gray-800">
