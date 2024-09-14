7f9a394f29bf17fcb1f62f2f2435d9ef7a5018ac
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2024-09-14 17:09:17 +0100

    completed funding page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,179 +1,4 @@
 @extends('layouts.new-guest')
-
-@section('head')
-    {{-- 
-<link rel="stylesheet" href="{{asset('css/index.css')}}" />
-<link rel="stylesheet" href="{{asset('css/dashboard_sidebar.css')}}" />
-<link rel="stylesheet" href="{{asset('css/dashboard_index.css')}}" />
-
-<style>
-    .dashboard_body {
-        display: flex;
-        flex-direction: row;
-        gap: 2rem;
-        margin-top: 3rem;
-
-        /* align-items: center; */
-    }
-</style>
-@endsection
-
-@section('body')
-<div class="dashboard_body">
-    <div class="sidebar_body">
-        @include('components.dasboard_sidebar')
-    </div>
-
-    <div class="dashboard_section">
-        <div class="dashboard-container">
-            <h1>Dashboard</h1>
-            <div class="row">
-                <div class="mb-3 col-12 col-lg-4 col-xl-4 col-md-4">
-                    <div class="card">
-                        <div class="card-body text-center">
-                            <div class="card-content">
-                                <p>Wallet Balance</p>
-                                <h3>₦ {{ number_format(auth()->user()->account_balance, 2) }}</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="mb-3 col-12 col-lg-4 col-xl-4 col-md-4">
-                    <div class="card">
-                        <div class="card-body text-center">
-                            <div class="card-content">
-                                <p>Referral Bonus</p>
-                                <h3>₦ 0.0</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="mb-3 col-12 col-lg-4 col-xl-4 col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="card-content">
-                                <p>My Total Referral</p>
-                                <h3>0</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="dashboard-container">
-            <h1>Services</h1>
-            <div class="row">
-                <div class="mb-3 col-6 col-md-3">
-                    <a href="{{ route('airtime.index') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Buy Airtime</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="mb-3 col-6 col-md-3">
-                    <a href="{{ route('data.index') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Buy Data</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="mb-3 col-6 col-md-3">
-                    <a href="{{ route('electricity.index') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Electricity</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="mb-3 col-6 col-md-3">
-                    <a href="{{ route('cable.index') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Cable</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <div class="dashboard-container">
-            <h1>Transaction History</h1>
-            <div class="row">
-                <div class="mb-3 col-6 col-md-4">
-                    <a href="{{ route('user.transaction.airtime') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Airtime Transactions</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="mb-3 col-6 col-md-4">
-                    <a href="{{ route('user.transaction.data') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Data Transactions</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="mb-3 col-6 col-md-4">
-                    <a href="{{ route('user.transaction.electricity') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Electricity Transactions</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="mb-3 col-6 col-md-4">
-                    <a href="{{ route('user.transaction.cable') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Cable Transactions</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="mb-3 col-6 col-md-4">
-                    <a href="{{ route('user.transaction.education') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Education Transactions</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="mb-3 col-6 col-md-4">
-                    <a href="{{ route('user.transaction.wallet') }}">
-                        <div class="card">
-                            <div class="card-body text-center">
-                                <h6>Wallet Summary</h6>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <!-- card indicators -->
-        <div class="d-flex box_container">
-
-        </div>
-        <!-- end of card indicators -->
-    </div>
-
-</div>
-@endsection --}}
 @section('body')
     <!-- Wallet Balance -->
     <div class="px-[2rem] bg-white  shadow-xl p-6 mb-8">
@@ -291,15 +116,16 @@
                 <div class="p-6">
                     <h5 class="text-sm font-medium text-gray-600">Virtual accounts</h5>
                     <p class="mb-4 text-xs text-gray-500">Make a transfer to any of the accounts</p>
+                    @php $count = 0 @endphp
                     @foreach (auth()->user()->virtualAccounts()->get() as $key => $account)
-                        <!-- Account {{ ++$key }} -->
+                        <!-- Account {{ ++$count }} -->
                         <div class="flex items-center justify-between p-4 mb-3 bg-gray-100 rounded-lg">
                             <div>
                                 <p class="text-sm font-medium text-gray-800">{{ $account->account_name }}</p>
                                 {{-- <p class="text-xs text-gray-500">Account Name</p> --}}
-                                <p  class="text-sm font-medium text-gray-800 account-number">{{ $account->account_number }}</p>
+                                <p  class="text-sm font-medium text-gray-800 account-number" id="account-number-{{ $count }}">{{ $account->account_number }}</p>
                             </div>
-                            <button class="text-indigo-600 text-sm font-medium copy-button">Copy</button>
+                            <button type="button" class="text-indigo-600 text-sm font-medium copy-button" data-target="account-number-{{ $count }}">Copy</button>
                         </div>
                     @endforeach
 
@@ -319,26 +145,36 @@
 @endsection
 @push('scripts')
     <script>
-        let button = document.querySelector(".copy-button");
-        let textToCopy = document.querySelector(".account-number");
+        let buttons = document.querySelectorAll(".copy-button");
 
-        button.addEventListener("click", () => {
-            const storage = document.createElement('textarea');
-            storage.value = textToCopy.innerHTML;
-            textToCopy.appendChild(storage);
-            storage.select();
-            storage.setSelectionRange(0, 99999);
-            try {
-                const successful = document.execCommand('copy');
-                if (successful) {
-                    toastr.success('Account No. Copied Successfully!');
+        buttons.forEach(button => {
+            button.addEventListener("click", () => {
+                let targetId = button.getAttribute('data-target');
+                let textToCopy = document.getElementById(targetId);
+
+                if (textToCopy) {
+                    const storage = document.createElement('textarea');
+                    storage.value = textToCopy.innerText;
+                    document.body.appendChild(storage);
+                    storage.select();
+                    storage.setSelectionRange(0, 99999);
+
+                    try {
+                        const successful = document.execCommand('copy');
+                        if (successful) {
+                            toastr.success('Account No. Copied Successfully!');
+                        } else {
+                            toastr.warning('Failed to copy!');
+                        }
+                    } catch (err) {
+                        toastr.error('Failed to copy!');
+                    } finally {
+                        document.body.removeChild(storage);
+                    }
                 } else {
-                    toastr.warning('Failed to copy!');
+                    toastr.warning('Target element not found!');
                 }
-            } catch (err) {
-                alert('Failed to copy!');
-            }           
-            textToCopy.removeChild(storage);
+            });
         });
     </script>
 @endpush
--- resources/views/pages/payment/index.blade.php
+++ resources/views/pages/payment/index.blade.php
@@ -1,47 +1,72 @@
 @extends('layouts.new-guest')
 @section('body')
-<div class="max-w-lg w-full bg-white p-8 ">
-    <div class="flex items-center mb-6">
-        <a href="{{ route('dashboard') }}" class="text-blue-600">
-            <i class="fas fa-arrow-left"></i> Back
-        </a>
-    </div>
-    <h2 class="text-2xl font-bold mb-6">Fund Your Wallet</h2>
-    <form action="{{ route('payment.process') }}" method="GET">
-        @csrf    
-        <div class="relative z-0 mb-6 w-full group mt-4">
-            <input type="number" wire:model="amount" name="amount" id="amount" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" />
-            <input type="hidden" value="{{ auth()->user()->username }}" name="user">
-            <label for="amount" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter Amount</label>
-            <i class="fas fa-money-bill-wave absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-400"></i>
-            @error('amount')<span class="text-red-500 font-bold text-sm"> {{ $message }} </span>@enderror
+    <div class="max-w-lg w-full bg-white p-8 ">
+        <div class="flex items-center mb-6">
+            <a href="{{ route('dashboard') }}" class="text-blue-600">
+                <i class="fas fa-arrow-left"></i> Back
+            </a>
         </div>
-
-        <div class="relative z-0 mb-6 w-full group mt-4">
-            {{-- <h6 class="mb-3">Choose Payment Gateway</h6> --}}
-            <div class="flex align-middle">
-                @foreach ($payments as $payment)
-                    <div class="me-3">
-                        <input type="radio" name="gateway" id="{{ Str::lower($payment->name) }}" value="{{ Str::lower($payment->name) }}" {{ $loop->first ? 'checked' : '' }}>
-                    </div>
-                    <div class="me-3">
-                        <label for="{{ Str::lower($payment->name) }}">
-                            <img src="{{ asset('images/' . Str::lower($payment->name) . '.png') }}" width="100" alt="">
-                        </label>
-                    </div>
-                @endforeach
+        <h2 class="text-2xl font-bold mb-6">Fund Your Wallet</h2>
+        <form action="{{ route('payment.process') }}" method="GET">
+            @csrf
+            <div class="relative z-0 mb-6 w-full group mt-4">
+                <input type="number" wire:model="amount" name="amount" id="amount"
+                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" />
+                <input type="hidden" value="{{ auth()->user()->username }}" name="user">
+                <label for="amount"
+                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter
+                    Amount</label>
+                <i class="fas fa-money-bill-wave absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-400"></i>
+                @error('amount')
+                    <span class="text-red-500 font-bold text-sm"> {{ $message }} </span>
+                @enderror
             </div>
-        </div>
-        
-        <button type="submit" class="w-full bg-vastel_blue text-white py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:bg-blue-700">
-            <span wire:loading.remove wire:target='validateForm'>Proceed</span>
-        </button>
 
-        @if (!count($accounts))
-            @livewire('pages.virutal-account.create', [App\Models\User::find(auth()->user()->id)])
-        @endif
+            <div class="relative z-0 mb-6 w-full group mt-4">
+                {{-- <h6 class="mb-3">Choose Payment Gateway</h6> --}}
+                <div class="flex align-middle">
+                    @foreach ($payments as $payment)
+                        <div class="me-3">
+                            <input type="radio" name="gateway" id="{{ Str::lower($payment->name) }}"
+                                value="{{ Str::lower($payment->name) }}" {{ $loop->first ? 'checked' : '' }}>
+                        </div>
+                        <div class="me-3">
+                            <label for="{{ Str::lower($payment->name) }}">
+                                <img src="{{ asset('images/' . Str::lower($payment->name) . '.png') }}" width="100"
+                                    alt="">
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
 
-        {{-- @if (count($accounts))
+            <button type="submit"
+                class="w-full bg-vastel_blue text-white py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:bg-blue-700">
+                <span wire:loading.remove wire:target='validateForm'>Proceed</span>
+            </button>
+
+            @if (count($accounts))
+                <div class="mt-6">
+                    <h5 class="text-sm font-medium text-gray-600">Virtual accounts</h5>
+                    <p class="mb-4 text-xs text-gray-500">Make a transfer to any of the accounts</p>
+                    @php $count = 0 @endphp
+                    @foreach (auth()->user()->virtualAccounts()->get() as $key => $account)
+                        <!-- Account {{ ++$count }} -->
+                        <div class="flex items-center justify-between p-4 mb-3 bg-gray-100 rounded-lg">
+                            <div>
+                                <p class="text-sm font-medium text-gray-800">{{ $account->account_name }}</p>
+                                {{-- <p class="text-xs text-gray-500">Account Name</p> --}}
+                                <p  class="text-sm font-medium text-gray-800 account-number" id="account-number-{{ $count }}">{{ $account->account_number }}</p>
+                            </div>
+                            <button type="button" class="text-indigo-600 text-sm font-medium copy-button" data-target="account-number-{{ $count }}">Copy</button>
+                        </div>
+                    @endforeach
+                </div>
+            @else
+                @livewire('pages.virutal-account.create', [App\Models\User::find(auth()->user()->id)])
+            @endif
+
+            {{-- @if (count($accounts))
         <div class="row">
             <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
                 <div class="dashboard-container ml-0">
@@ -68,6 +93,41 @@
         @else
             @livewire('pages.virutal-account.create', [App\Models\User::find(auth()->user()->id)])
         @endif --}}
-    </form>
-</div>
-@endsection 
+        </form>
+    </div>
+@endsection
+@push('scripts')
+    <script>
+        let buttons = document.querySelectorAll(".copy-button");
+
+        buttons.forEach(button => {
+            button.addEventListener("click", () => {
+                let targetId = button.getAttribute('data-target');
+                let textToCopy = document.getElementById(targetId);
+
+                if (textToCopy) {
+                    const storage = document.createElement('textarea');
+                    storage.value = textToCopy.innerText;
+                    document.body.appendChild(storage);
+                    storage.select();
+                    storage.setSelectionRange(0, 99999);
+
+                    try {
+                        const successful = document.execCommand('copy');
+                        if (successful) {
+                            toastr.success('Account No. Copied Successfully!');
+                        } else {
+                            toastr.warning('Failed to copy!');
+                        }
+                    } catch (err) {
+                        toastr.error('Failed to copy!');
+                    } finally {
+                        document.body.removeChild(storage);
+                    }
+                } else {
+                    toastr.warning('Target element not found!');
+                }
+            });
+        });
+    </script>
+@endpush
