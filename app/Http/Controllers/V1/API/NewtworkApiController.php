5882c31b67e68192fb53367c093ab3cd87d21b19
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2024-09-21 19:34:42 +0100

    add queryParams to network API

--- app/Http/Controllers/V1/API/NewtworkApiController.php
+++ app/Http/Controllers/V1/API/NewtworkApiController.php
@@ -2,32 +2,34 @@
 
 namespace App\Http\Controllers\V1\API;
 
-use App\Http\Controllers\Controller;
+use App\Helpers\ApiHelper;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\Data\DataVendor;
 use App\Models\Data\DataNetwork;
+use App\Http\Controllers\Controller;
 use App\Traits\ResolvesVendorService;
 
 class NewtworkApiController extends Controller
 {
     use ResolvesVendorService;
-    
-    public function index()
+
+    public function index(Request $request)
     {
+        $args = ['data', 'airtime'];
+
+        if (!$request->query('network') || !in_array($request->query('network'), $args)) {
+            return ApiHelper::sendError([], 'Network not found');
+        }
+
+        $queryNetwork = Str::lower($request->query('network'));
+        
         try {
-            $vendor = $this->getVendorService('data');
+            $vendor = $this->getVendorService($queryNetwork);
             $network = DataNetwork::whereVendorId($vendor?->id)->whereStatus(true)->get();
-            return response()->json([
-                'status'    =>  'success', 
-                'message'   =>  "Network Fetched Successfully",
-                'response'  =>  $network
-            ]);
+            return ApiHelper::sendResponse($network, 'Networks Fetched Successfully');
         } catch (\Throwable $th) {
-            return response()->json([
-                'status'  => 'failed', 
-                'message' =>  "Unable to fetch network. Try again later",
-                'error'   =>  $th->getMessage()
-            ]);
+            return ApiHelper::sendError([$th->getMessage()], 'Unable to fetch network. Try again later');
         }
     }
 }
