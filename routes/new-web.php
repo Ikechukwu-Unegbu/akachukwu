ad0a961d4461e1bb0d15ee41a07832a1853f3099
Ikechukwu-Unegbu <iunegbu94@yahoo.com>
2024-09-28 05:37:07 +0100

    yesterday work on feature UI migration

--- routes/new-web.php
+++ routes/new-web.php
@@ -10,16 +10,11 @@ Route::prefix('new')->group(function () {
 
 
 
-   Route::view('dashboard/services/otp', 'new.otp');
+   Route::view('dashboard/services/kyc', 'new.kyc');
    Route::view('dashboard/services/status', 'new.status_modal');
 
 
-   Route::view('/dashboard/profile', 'new.profile');
-   Route::view('/dashboard/support', 'new.support')->name('new.support');
 
-   //settings 
-   Route::view('/dashboard/settings', 'new.settings')->name('new.setting');
-   Route::view('/dashboard/security', 'new.security')->name('new.security');
-   Route::view('/dashboard/referral', 'new.referral')->name('new.referral');
+
 
 });
--- routes/web.php
+++ routes/web.php
@@ -93,7 +93,6 @@ Route::middleware(['auth', 'impersonate'])->group(function () {
     Route::get('/profile', [ProfileSettingsController::class, 'edit'])->name('profile.edit');
     Route::get('/pins', [ProfileSettingsController::class, 'editPin'])->name('profile.pin');
     Route::post('/profile', [ProfileSettingsController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::post('update-password', [SettingsController::class, 'updatePassword'])->name('update.password');
