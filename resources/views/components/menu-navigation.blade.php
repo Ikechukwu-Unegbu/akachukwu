700db749b2a3d439ce0bdd3d56bf92b8673a7425
Ikechukwu-Unegbu <iunegbu94@yahoo.com>
2024-10-02 05:21:07 +0100

    M

--- resources/views/components/menu-navigation.blade.php
+++ resources/views/components/menu-navigation.blade.php
@@ -36,27 +36,20 @@
                 <span class="sr-only">Close menu</span>
             </button>
                 
-            <!-- <p class="mb-6 text-sm text-gray-500 dark:text-gray-400">Supercharge your hiring by taking advantage of our <a href="#" class="text-blue-600 underline dark:text-vastel_blue hover:no-underline">limited-time sale</a> for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design job board.</p> -->
-            <!-- <div class="grid grid-cols-2 gap-4">
-                <a href="#" class="px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Learn more</a>
-                <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get access <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg></a>
-            </div> -->
+          
             <div class="off-canvas-nav  text-vastel_blue p-4">
                 <ul class="flex flex-col gap-[1rem]">
                     <li><a href="/" class="text-vastel_blue hover:text-vastel_blue">Home</a></li>
-                    <li><a href="{{ route('airtime.index') }}" class="text-vastel_blue hover:text-vastel_blue">Airtime</a></li>
+                    <!-- <li><a href="{{ route('airtime.index') }}" class="text-vastel_blue hover:text-vastel_blue">Airtime</a></li>
                     <li><a href="{{ route('data.index') }}" class="text-vastel_blue hover:text-vastel_blue">Internet Data</a></li>
-                    <li><a href="{{ route('education.result.index') }}" class="text-vastel_blue hover:text-vastel_blue">Result Checker</a></li>
+                    -->
                     <li>
                         <a href="{{ route('dashboard') }}" class="text-vastel_blue hover:text-vastel_blue">
                             @guest Bills Payment @else Dashboard @endguest
                         </a>
                     </li>
                     <li><a href="{{ route('profile.edit') }}" class="text-vastel_blue hover:text-vastel_blue">Profile</a></li>
-                    <li><a href="{{ route('payment.index') }}" class="text-vastel_blue hover:text-vastel_blue">Fund Account</a></li>
-
+    
                     @guest
                     <li>
                         <ul class="flex flex-col gap-[1rem]">
--- resources/views/layouts/new-guest.blade.php
+++ resources/views/layouts/new-guest.blade.php
@@ -193,10 +193,8 @@
  
 
     <!-- off can vas -->
-    <div id="drawer-example" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-label">
-        <h5 id="drawer-label" class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"><svg class="w-4 h-4 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-        </svg>Info</h5>
+    {{--<div id="drawer-example" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-label">
+        <img class="h-[4rem] w-[4rem]" src="{{asset('images/vastel-logo.svg')}}" alt="">
         <button type="button" data-drawer-hide="drawer-example" aria-controls="drawer-example" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white" >
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -211,28 +209,20 @@
             </a>
             <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
                 <i class="fas fa-phone-alt"></i>
-                <span>Airtime</span>
+                <span>
+                    About 
+                </span>
             </a>
             <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
-                <i class="fas fa-wifi"></i>
-                <span>Internet Data</span>
+                <i class=" fa-solid fa-cubes-stacked"></i>
+                <span>Services</span>
             </a>
-            <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
-                <i class="fas fa-check"></i>
-                <span>Result Checker</span>
-            </a>
+        
             <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
                 <i class="fas fa-tachometer-alt"></i>
                 <span>Dashboard</span>
             </a>
-            <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
-                <i class="fas fa-user"></i>
-                <span>Profile</span>
-            </a>
-            <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
-                <i class="fas fa-wallet"></i>
-                <span>Fund Account</span>
-            </a>
+     
             <a href="#" class="flex items-center space-x-2 text-white bg-blue-700 rounded-md p-2">
                 <i class="fas fa-sign-out-alt"></i>
                 <span>Logout</span>
@@ -247,7 +237,43 @@
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
         </svg></a>
         </div> -->
-    </div>
+    </div>--}}
+    <div id="drawer-example" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80" tabindex="-1" aria-labelledby="drawer-label">
+    <img class="h-[4rem] w-[4rem]" src="{{ asset('images/vastel-logo.svg') }}" alt="Vastel Logo">
+    
+    <button type="button" data-drawer-hide="drawer-example" aria-controls="drawer-example" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 flex items-center justify-center">
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+        </svg>
+        <span class="sr-only">Close menu</span>
+    </button>
+
+    <nav class="flex flex-col text-blue-800 space-y-2 p-4">
+        <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
+            <i class="fas fa-home"></i>
+            <span>Home</span>   
+        </a>
+        <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
+            <i class="fas fa-phone-alt"></i>
+            <span>About</span>
+        </a>
+        <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
+            <i class="fa-solid fa-cubes-stacked"></i>
+            <span>Services</span>
+        </a>
+    
+        <a href="#" class="flex items-center space-x-2 hover:text-blue-500">
+            <i class="fas fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+
+        <a href="#" class="flex items-center space-x-2 text-white bg-blue-700 rounded-md p-2">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Logout</span>
+        </a>
+    </nav>
+</div>
+
     <!-- end off can vas -->
    
     <x-toastr />
