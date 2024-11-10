88617fbc5b54d1fc3c4923f52169bfadfbd194af
Ikechukwu-Unegbu <iunegbu94@yahoo.com>
2024-11-10 22:54:12 +0100

    progress

--- resources/views/livewire/admin/hr/user/show.blade.php
+++ resources/views/livewire/admin/hr/user/show.blade.php
@@ -127,7 +127,7 @@
             </div>
             <div class="card-body">
                 <x-table>
-                    <x-table-header :headers="['#', 'Reference', 'Type', 'Amount', 'Date', 'Status']" />
+                    <x-table-header :headers="['#', 'Reference', 'Type','', 'Amount', 'Date', 'Status']" />
                     <x-table-body>
                         @forelse ($user->transactionHistories(10) as $transaction)
                             <tr>
