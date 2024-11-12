2376c55a429258b37c5050c4383a3d7ce81c9b10
Ikechukwu-Unegbu <iunegbu94@yahoo.com>
2024-11-12 14:57:38 +0100

    money transfer modal done

---
+++ app/Livewire/Component/MoneyTransferComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Component;
+
+use Livewire\Component;
+
+class MoneyTransferComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.component.money-transfer-component');
+    }
+}
