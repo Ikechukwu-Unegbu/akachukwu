d5c81d47b266cd296786c3f79db93666496932bc
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2024-11-19 11:51:31 +0100

    fixed image upload issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,8 @@
 
 namespace App\Providers;
 
-use Livewire\Livewire;
 use App\Models\SiteSetting;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,17 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::share('settings', SiteSetting::find(1));
-
-        Livewire::setUpdateRoute(function ($handle) {
-            $path = config('app.path').'/livewire/update';
-            return Route::post($path, $handle)->middleware('web');
-        });
-        Livewire::setScriptRoute(function ($handle) {
-            $path = config('app.path').'/livewire/livewire.js';
-            return Route::get($path, $handle)->middleware('web');
-        });
-        URL::forceRootUrl(config('app.url'));
-        URL::forceScheme(config('app.scheme','http'));
     }
     
 }
