822cf589928c50616bb3c8b5176a33154d696499
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2024-12-12 09:14:01 +0100

    Secure user transaction history query by using parameterized queries

--- app/Livewire/Admin/Wallet/Index.php
+++ app/Livewire/Admin/Wallet/Index.php
@@ -23,7 +23,7 @@ class Index extends Component
     public function render()
     {
         return view('livewire.admin.wallet.index', [
-            'walletHistories' => $this->user->transactionHistories($this->perPage)
+            'walletHistories' => $this->user->checkUserTransactionHistories($this->perPage, $this->user->id)
         ]);
     }
 }
