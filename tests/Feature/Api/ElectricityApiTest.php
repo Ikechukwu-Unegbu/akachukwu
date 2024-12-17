69c810b95e638fa7b839c509b82ca2d77d36941a
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2024-12-17 16:51:22 +0100

    completed ElectricityTestApi

--- tests/Feature/Api/ElectricityApiTest.php
+++ tests/Feature/Api/ElectricityApiTest.php
@@ -17,9 +17,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ElectricityApiTest extends TestCase
 {
     use RefreshDatabase;
-    protected CONST METER_NUMBER = '1111111111111';
-    protected CONST DISCO_ID = 'ikeja-electric';
-    protected CONST METER_TYPE = 1;
 
     public function setup() : void
     {
@@ -34,21 +31,147 @@ class ElectricityApiTest extends TestCase
     /** @test electricity purchase fails when user has insufficient balance */
     public function electricity_purchase_fails_when_user_has_insufficient_balance()
     {
-        $user = User::factory()->create([
-            'account_balance' => 10,
-        ]);
+        try {
+            $user = User::factory()->create([
+                'account_balance' => 10,
+            ]);
 
-        $response = $this->actingAs($user)->postJson('/api/electricity/validate', [
-            'meter_number' => self::METER_NUMBER,
-            'disco_id'    => self::DISCO_ID,
-            'meter_type' =>  self::METER_TYPE,
-        ]);
+            // Set up test data
+            $requestPayload = [
+                'amount'        => 1000,
+                'meter_number'  => '1111111111111',
+                'disco_id'      => 'ikeja-electric',
+                'meter_type'    => '1',
+                'phone_number'  => '08130974397',
+            ];
 
-        // if ($response->assertSt)
+            $validateResponse = $this->actingAs($user)->postJson('/api/electricity/validate', [
+                'meter_number' => $requestPayload['meter_number'],
+                'disco_id'     => $requestPayload['disco_id'],
+                'meter_type'   => $requestPayload['meter_type'],
+            ]);
 
-        // $response->assertStatus(422);
+            $validateResponse->assertStatus(200)->assertJsonStructure([
+               'status',
+               'response' => ['name', 'address'],
+               'message'
+            ]);
 
-        Log::info(json_encode($response));
-        
+            $validatedData = $validateResponse->json('response');
+           
+            $purchaseResponse = $this->actingAs($user)->postJson('/api/electricity/create', array_merge($requestPayload, [
+                'owner_name'    => $validatedData['name'],
+                'owner_address' => $validatedData['address'],
+            ]));
+
+            $purchaseResponse->assertJson([
+                'status' => false,
+                'message' => 'Your account balance is insufficient to complete this transaction.',
+            ]);
+
+        } catch (\Exception $e) {
+            Log::error("electricity purchase fails when user has insufficient balance",  [
+                'message' => $e->getMessage(),
+                'trace'   => $e->getTraceAsString(),
+            ]);
+            throw $e;
+        }
+    }
+
+    /** @test electricity purchase is successful when user has sufficient balance */
+    public function electricity_purchase_is_successful_when_user_has_sufficient_balance()
+    {
+        try {
+            $user = User::factory()->create([
+                'account_balance' => 1000,
+            ]);
+
+            // Set up test data
+            $requestPayload = [
+                'amount'        => 500,
+                'meter_number'  => '1111111111111',
+                'disco_id'      => 'ikeja-electric',
+                'meter_type'    => '1',
+                'phone_number'  => '08130974397',
+            ];
+
+            $validateResponse = $this->actingAs($user)->postJson('/api/electricity/validate', [
+                'meter_number' => $requestPayload['meter_number'],
+                'disco_id'     => $requestPayload['disco_id'],
+                'meter_type'   => $requestPayload['meter_type'],
+            ]);
+
+            $validateResponse->assertStatus(200)->assertJsonStructure([
+               'status',
+               'response' => ['name', 'address'],
+               'message'
+            ]);
+
+            $validatedData = $validateResponse->json('response');
+           
+            $purchaseResponse = $this->actingAs($user)->postJson('/api/electricity/create', array_merge($requestPayload, [
+                'owner_name'    => $validatedData['name'],
+                'owner_address' => $validatedData['address'],
+            ]));
+
+            $purchaseResponse->assertStatus(200);
+
+        } catch (\Exception $e) {
+            Log::error("electricity purchase is successful when user has sufficient balance",  [
+                'message' => $e->getMessage(),
+                'trace'   => $e->getTraceAsString(),
+            ]);
+            throw $e;
+        }
+    }
+
+    /** @test electricity purchase returns valid details */
+    public function electricity_purchase_returns_valid_details()
+    {
+        try {
+            $user = User::factory()->create([
+                'account_balance' => 1000,
+            ]);
+
+            // Set up test data
+            $requestPayload = [
+                'amount'        => 500,
+                'meter_number'  => '1111111111111',
+                'disco_id'      => 'ikeja-electric',
+                'meter_type'    => '1',
+                'phone_number'  => '08130974397',
+            ];
+
+            $validateResponse = $this->actingAs($user)->postJson('/api/electricity/validate', [
+                'meter_number' => $requestPayload['meter_number'],
+                'disco_id'     => $requestPayload['disco_id'],
+                'meter_type'   => $requestPayload['meter_type'],
+            ]);
+
+            $validateResponse->assertStatus(200)->assertJsonStructure([
+               'status',
+               'response' => ['name', 'address'],
+               'message'
+            ]);
+
+            $validatedData = $validateResponse->json('response');
+           
+            $purchaseResponse = $this->actingAs($user)->postJson('/api/electricity/create', array_merge($requestPayload, [
+                'owner_name'    => $validatedData['name'],
+                'owner_address' => $validatedData['address'],
+            ]));
+
+            $purchaseResponse->assertStatus(200)->assertJson([
+                'status' => true,
+                'response' => [],
+            ]);
+
+        } catch (\Exception $e) {
+            Log::error("electricity purchase is successful when user has sufficient balance",  [
+                'message' => $e->getMessage(),
+                'trace'   => $e->getTraceAsString(),
+            ]);
+            throw $e;
+        }
     }
 }
