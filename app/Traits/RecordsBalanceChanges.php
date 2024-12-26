2cacf5b7164e9a33ffbe22c67e4d2a2f00a087cd
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2024-12-26 21:43:37 +0100

    add balance before and after to funding

--- app/Traits/RecordsBalanceChanges.php
+++ app/Traits/RecordsBalanceChanges.php
@@ -4,20 +4,22 @@ namespace App\Traits;
 
 trait RecordsBalanceChanges
 {
+    const STATUS_ACTIVE = 1;
+
     protected static function bootRecordsBalanceChanges()
     {
         static::creating(function ($model) {
             $user = auth()->user();
             $model->balance_before = $user->account_balance;
             $model->balance_after = $model->balance_before;
-            if ($model->status === 1 && self::where('reference_id', $model->reference_id)->exists()) {
+            if ($model->status === self::STATUS_ACTIVE && self::where('reference_id', $model->reference_id)->exists()) {
                 $model->balance_after = $model->balance_before + $model->amount;
             }
         });
         
         static::updating(function ($model) {
             $user = auth()->user();
-            if ($model->status === 1 && self::where('reference_id', $model->reference_id)->where('user_id', $user->id)->exists()) {
+            if ($model->status === self::STATUS_ACTIVE && self::where('reference_id', $model->reference_id)->where('user_id', $user->id)->exists()) {
                 $model->balance_after = $model->balance_before + $model->amount;
             }
         });
