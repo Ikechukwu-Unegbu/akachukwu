d96d1bff64a589663bb4b382f30ff2c5a4480fed
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2024-12-30 18:01:19 +0100

    fixed static account not funding

--- app/Traits/RecordsBalanceChanges.php
+++ app/Traits/RecordsBalanceChanges.php
@@ -12,26 +12,28 @@ trait RecordsBalanceChanges
     {
         static::creating(function ($model) {
             $user = auth()->user();
-
-            $model->balance_before = $user->account_balance;
-            $model->balance_after = $model->balance_before;
-
-            if ($model->status === self::STATUS_ACTIVE && self::where('reference_id', $model->reference_id)->exists()) {
-                $model->balance_after = $model->balance_before + $model->amount;
+            if ($user) {
+                $model->balance_before = $user->account_balance;
+                $model->balance_after = $model->balance_before;
+    
+                if ($model->status === self::STATUS_ACTIVE && self::where('reference_id', $model->reference_id)->exists()) {
+                    $model->balance_after = $model->balance_before + $model->amount;
+                }
             }
         });
 
         static::updating(function ($model) {
             DB::transaction(function () use ($model) {
                 $user = auth()->user();
-
-                $existingRecord = self::where('reference_id', $model->reference_id)
-                    ->where('user_id', $user->id)
-                    ->lockForUpdate()
-                    ->first();
-
-                if ($model->status === self::STATUS_ACTIVE && $existingRecord) {
-                    $model->balance_after = $existingRecord->balance_after + $model->amount;
+                if ($user) {
+                    $existingRecord = self::where('reference_id', $model->reference_id)
+                        ->where('user_id', $user->id)
+                        ->lockForUpdate()
+                        ->first();
+    
+                    if ($model->status === self::STATUS_ACTIVE && $existingRecord) {
+                        $model->balance_after = $existingRecord->balance_after + $model->amount;
+                    }
                 }
             });
         });
