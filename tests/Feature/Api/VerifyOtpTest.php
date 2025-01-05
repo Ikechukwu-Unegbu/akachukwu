a1db91b880102bd52fe24ca4f43004e06b6112e1
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2025-01-05 11:39:00 +0100

    implemented test for OTP endpoints

---
+++ tests/Feature/Api/VerifyOtpTest.php
@@ -0,0 +1,72 @@
+<?php
+
+use App\Models\Otp;
+use Tests\TestCase;
+use App\Models\User;
+use App\Services\OTPService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Log;
+
+class VerifyOtpTest extends TestCase
+{
+    use RefreshDatabase;
+
+    use RefreshDatabase;
+
+    protected $user;
+    protected $otp;
+    protected OTPService $otpService;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create([
+            'email' => 'user@example.com',
+            'password' => bcrypt('password123'),
+        ]);
+
+        $this->otp = (new OTPService())->generateOTP($this->user);
+    }
+
+    /** @test that OTP verification fails with incorrect OTP. */
+    public function test_verify_otp_fails_with_incorrect_otp()
+    {
+        $response = $this->postJson('/api/verify-otp', [
+            'email' => $this->user->email,
+            'otp' => '0000x',
+        ]);
+
+        $response->assertStatus(403)
+                    ->assertJson([
+                        'message' => 'Invalid otp',
+                    ]);
+    }
+
+    /** @test that OTP verification succeeds with correct OTP. */
+    public function test_verify_otp_succeeds_with_correct_otp()
+    {
+        $response = $this->postJson('/api/verify-otp', [
+            'email' => $this->user->email,
+            'otp' => (string) $this->otp,
+        ]);
+
+        $response->assertStatus(200)
+                 ->assertJson([
+                     'message' => 'Valid Otp',
+                 ]);
+    }
+
+    /** @test that OTP verification succeeds with correct OTP. */
+    public function test_resend_otp_succeeds()
+    {
+        $response = $this->postJson('/api/resend-otp', [
+            'email' => $this->user->email
+        ]);
+
+        $response->assertStatus(200)
+                 ->assertJson([
+                    'status'=>'success'
+                 ]);
+    }
+}
