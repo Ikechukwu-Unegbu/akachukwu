6bc168f7d5c048053fba8f60ceac78f7b755517f
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2025-01-28 10:20:56 +0100

    fixed b4 & bafter on Bonus Withdrawal

--- app/Http/Controllers/V1/BonusWithdrawalController.php
+++ app/Http/Controllers/V1/BonusWithdrawalController.php
@@ -13,9 +13,8 @@ class BonusWithdrawalController extends Controller
     {
         DB::transaction(function () use ($request) {
             $user = DB::table('users')->where('id', $request->user()->id)->lockForUpdate()->first();
-
             if ($user->bonus_balance > 0) {
-                VastelTransaction::create([
+                $transaction = VastelTransaction::create([
                     'reference_id' => 'VST-'.random_int(100000000000, 999999999999),
                     'amount'=>$user->bonus_balance,
                     'type'=>1,
@@ -25,14 +24,16 @@ class BonusWithdrawalController extends Controller
                     'user_id'=>$user->id, 
                     'admin_id'=>0,
                     'description'=>'Bonus withdrawal'
-                ]);
+                ]);               
+
                 DB::table('users')
                     ->where('id', $user->id)
                     ->update([
                         'bonus_balance' => 0,
                         'account_balance' => DB::raw("account_balance + {$user->bonus_balance}")
                     ]);
-                 
+
+                $transaction->success();
             } else {
                 return redirect()->route('settings.referral', ['modal' => 'successful']);
 
