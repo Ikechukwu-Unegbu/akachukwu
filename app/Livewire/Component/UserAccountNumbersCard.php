01bce527e408cf0c21d35f69f92085782f754547
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2025-02-12 10:59:30 +0100

    fixed admin can create virutal account for user

--- app/Livewire/Component/UserAccountNumbersCard.php
+++ app/Livewire/Component/UserAccountNumbersCard.php
@@ -43,18 +43,22 @@ class UserAccountNumbersCard extends Component
 
     public function createVirtualAccount($bankCode)
     {
-        $service = (new GenerateRemainingAccounts)->generateSpecificAccount($bankCode);
+        try {
+            $service = (new GenerateRemainingAccounts)->generateSpecificAccount($bankCode);
 
-        if (isset($service->status) && $service->status === true) {
-            $this->dispatch('success-toastr', ['message' => $service->message]);
-            session()->flash('success', $service->message);
+            if (isset($service->status) && $service->status === true) {
+                $this->dispatch('success-toastr', ['message' => $service->message]);
+                session()->flash('success', $service->message);
+                return $this->redirect(url()->previous());
+            }
+
+            $errorMessage = "Failed to create virtual account. Please try again.";
+            $this->dispatch('error-toastr', ['message' => $errorMessage]);
+            session()->flash('error', $errorMessage);
             return $this->redirect(url()->previous());
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
         }
-
-        $errorMessage = "Failed to create virtual account. Please try again.";
-        $this->dispatch('error-toastr', ['message' => $errorMessage]);
-        session()->flash('error', $errorMessage);
-        return $this->redirect(url()->previous());
     }
 
 
