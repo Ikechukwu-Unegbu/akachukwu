f0d274579cd692a14bd7fbcac4d574ac48d28c64
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2025-02-27 11:38:08 +0100

    completed webhook

--- app/Services/Money/PalmPayService.php
+++ app/Services/Money/PalmPayService.php
@@ -210,7 +210,65 @@ class PalmPayService
         ];
 
         self::storePayload($webhook);
-        return;
+
+        try {
+
+            $accountReference = $request->accountReference;
+            $headers = $request->headers->all();
+            $paymentReference = $request->orderNo;
+            $transactionReference = $request->sessionId;
+            $amountPaid = $request->orderAmount / 100;
+
+            if (PalmPayTransaction::where('reference_id', $paymentReference)->where('status', true)->exists()) {
+                return response()->json(['message' => 'Payment Already Processed'], 403);
+            }
+    
+            if ($request->appId === $headers['appid'][0] && $request->sign === $headers['sign'][0]) {
+                $virtualAccount = VirtualAccount::where('reference', $accountReference)->first();
+
+                if ($virtualAccount) {
+                    $user = $virtualAccount->user;
+
+                    $transaction = PalmPayTransaction::updateOrCreate([
+                        'reference_id'  => $paymentReference,
+                        'trx_ref'       => $transactionReference,
+                        'user_id'       => $user->id,
+                    ], [
+                        'amount'        => $amountPaid,
+                        'currency'      => config('app.currency', 'NGN'),
+                        'redirect_url'  => config('app.url'),
+                        'meta'          => json_encode($webhook)
+                    ]);
+
+                    $user->setAccountBalance($amountPaid);
+                    $transaction->success();
+
+                    return response()->json([
+                        'status'   =>    true,
+                        'error'    =>    NULL,
+                        'message'  =>    "Transaction successful",
+                        'response' =>    $transaction
+                    ], 200)->getData();
+                }
+            }
+    
+            return response()->json([
+                'status'   =>    false,
+                'error'    =>    "User not found",
+                'message'  =>    "Transaction not successful",
+                'response' =>    []
+            ], 200)->getData();
+            
+        } catch (\Throwable $th) {
+            $errors = [
+                'status'   =>    false,
+                'error'    =>    "Server Error",
+                'message'  =>    $th->getMessage(),
+                'response' =>    []
+            ];
+            Log::error($errors);
+            return response()->json($errors, 401)->getData();
+        }
     }
 
     public static function storePayload($payload)
