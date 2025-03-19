bf238c64ff02d59cc79577e84a5ae984d94e1501
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2025-03-19 12:51:53 +0100

    implemented one signal notification

--- app/Http/Controllers/V1/API/EducationController.php
+++ app/Http/Controllers/V1/API/EducationController.php
@@ -4,25 +4,26 @@ namespace App\Http\Controllers\V1\API;
 
 use App\Models\Vendor;
 use App\Helpers\ApiHelper;
+use App\Helpers\GeneralHelpers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Education\ResultChecker;
 use App\Services\OneSignalNotificationService;
 use App\Services\Education\ResultCheckerService;
 use App\Http\Requests\V1\Api\ResultCheckerRequest;
 use App\Models\Education\ResultCheckerTransaction;
 use App\Http\Resources\V1\API\ResultCheckerResource;
+use App\Notifications\EducationPinPurchaseNotification;
 
 class EducationController extends Controller
 {
     protected $vendor;
     protected $resultCheckerService;
-    protected $notificationService;
 
     public function __construct(Vendor $vendor, ResultCheckerService $resultCheckerService)
     {
         $this->vendor = $vendor->where('name', 'VTPASS')->first();
         $this->resultCheckerService = $resultCheckerService;
-        $this->notificationService = new OneSignalNotificationService();
     }
 
     public function index()
@@ -45,11 +46,15 @@ class EducationController extends Controller
             return ApiHelper::sendResponse($transactionResource, $response->message);
         }
 
-        $user = Auth::user();
+        $resultCheckerModel = ResultChecker::where('vendor_id', self::$vendor->id)->where('name', $request->exam_name)->first();
+        $amount = ($request->quantity * $resultCheckerModel->amount);
 
-        $subject = ($response->status) ? "Exam Token Purchase Successful" : "Bill Purchase Failed";
-
-        $this->notificationService->sendToUser($user, $subject, '');
+        GeneralHelpers::sendOneSignalTransactionNotification(
+            $response, 
+            $response->message, 
+            $amount, 
+            EducationPinPurchaseNotification::class
+        );
 
         return $response;
     }
