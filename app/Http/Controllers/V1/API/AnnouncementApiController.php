181ced1715abdb11dae6d104bf928c214264d7a5
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2025-04-28 13:11:48 +0100

    implement announcements api endpoint

---
+++ app/Http/Controllers/V1/API/AnnouncementApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\V1\API;
+
+use App\Helpers\ApiHelper;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\AnnouncementService;
+
+class AnnouncementApiController extends Controller
+{
+    protected $announcementService;
+
+    public function __construct(AnnouncementService $announcementService)
+    {
+        $this->announcementService = $announcementService;
+    }
+
+    public function show()
+    {
+        $announcements = $this->announcementService->getActiveAnnouncements();
+
+        return ApiHelper::sendResponse($announcements, 'Announcement Fetched Successfully');
+    }
+}
