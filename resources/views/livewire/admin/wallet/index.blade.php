fed11124bacd235dca1c2b00c11085715f6f0dcc
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2025-05-13 16:54:31 +0100

    fixed user view page

--- resources/views/livewire/admin/wallet/index.blade.php
+++ resources/views/livewire/admin/wallet/index.blade.php
@@ -7,7 +7,7 @@
     </x-admin.page-title>--}}
 
     <section class="section profile">
-    
+
         <div class="card mt-5">
             <div class="card-header">
                 <h4 class="card-title">Wallet History</h4>
@@ -15,30 +15,54 @@
             <div class="card-header">
                 <x-admin.perpage :perPages=$perPages wirePageAction="wire:model.live=perPage" wireSearchAction="wire:model.live=search"  />
             </div>
-            <div class="card-body">                        
+            <div class="card-body">
                 <x-table>
                     <x-table-header :headers="['#', 'Reference', 'Type', 'Amount', 'Bal B4', 'Bal After', 'Date', 'Status']" />
                     <x-table-body>
                         @forelse ($walletHistories as $wallet_transaction)
+
+                        @php
+                            $transactionId = $wallet_transaction->transaction_id;
+                            $balanceBefore = $wallet_transaction->balance_before;
+                            $balanceAfter = $wallet_transaction->balance_after;
+                            $status = $wallet_transaction->vendor_status;
+
+                            if ($wallet_transaction->title === 'Intra' || $wallet_transaction->title === 'Bank') {
+                                $moneyTransfer = \App\Models\MoneyTransfer::find($wallet_transaction->id);
+                                $transactionId = $moneyTransfer->reference_id;
+
+                                $status = $moneyTransfer->transfer_status;
+
+                                if ($user->id === $moneyTransfer->user_id) {
+                                    $balanceBefore = $moneyTransfer->sender_balance_before;
+                                    $balanceAfter = $moneyTransfer->sender_balance_after;
+                                } else {
+                                    $balanceBefore = $moneyTransfer->recipient_balance_before;
+                                    $balanceAfter = $moneyTransfer->recipient_balance_after;
+                                }
+                            }
+                        @endphp
+
                         <tr style="font-size: 10px;">
                             <th scope="row">{{ $loop->index + $walletHistories->firstItem() }}</th>
                             <td>
-                                <small>{{ $wallet_transaction->transaction_id }}</small>    
+                                <small>{{ $transactionId }} --- {{$wallet_transaction->title }}</small>
                             </td>
                             <td>{{ Str::title($wallet_transaction->utility) }}</td>
                             <td>₦ {{ number_format($wallet_transaction->amount, 2) }}</td>
-                            <td>₦ {{ isset($wallet_transaction->balance_before) ? $wallet_transaction->balance_before : 'NA' }}</td>
-                            <td>₦ {{ isset($wallet_transaction->balance_after) ? $wallet_transaction->balance_after : 'NA' }}</td>
+                            <td>₦ {{ $balanceBefore ?? 'N/A' }}</td>
+                            <td>₦ {{ $balanceAfter ?? 'NA' }}</td>
                             <td>
                                 {{ \Carbon\Carbon::parse($wallet_transaction->created_at)->format('M d, Y. h:ia') }}
                             </td>
                             <td>
-                                <span class="badge bg-{{ $wallet_transaction->status === 1 ? 'success' : ($wallet_transaction->status === 0 ? 'danger' : 'warning') }}">
-                                    {{ Str::title($wallet_transaction->vendor_status) }}
-                                    {{-- {{ $wallet_transaction->status === 1 ? 'Successful' : ($wallet_transaction->status === 0 ? 'Failed' : 'Refunded') }} --}}
-                                    {{-- @if($wallet_transaction->type==='vastel' && $wallet_transaction->type == false)
-                                        <small class="text-xs">Debit</small>
-                                    @endif  --}}
+                                <span class="badge
+                                        {{ $status === 'successful' ? 'bg-success' : '' }}
+                                        {{ $status === 'failed' ? 'bg-danger' : '' }}
+                                        {{ $status === 'pending' ? 'bg-warning' : '' }}
+                                        {{ $status === 'processing' ? 'bg-warning' : '' }}
+                                        {{ $status === 'refunded' ? 'bg-warning' : '' }}">
+                                    {{ ucfirst($status) }}
                                 </span>
                             </td>
                         </tr>
@@ -48,8 +72,8 @@
                         </tr>
                         @endforelse
                     </x-table-body>
-                </x-table>         
-                <x-paginate :paginate=$walletHistories />                   
+                </x-table>
+                <x-paginate :paginate=$walletHistories />
             </div>
         </div>
     </section>
