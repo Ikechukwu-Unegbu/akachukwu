704943d80d1f2589b39f10b0eb93aa4d297c59f2
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2025-05-27 08:35:26 +0100

    working on savings

---
+++ app/Services/Cowrywise/CowrywiseBaseService.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Services\Cowrywise;
+
+class CowrywiseBaseService
+{
+    protected static CONST SANDBOX_URL = "https://sandbox.embed.cowrywise.com/";
+    protected static CONST PRODUCTION_URL = "https://production.embed.cowrywise.com/";
+}
