151ab9ec2f4008d5d12dd586b3b22b05c3228881
Emmanuel Ademiju <ademijuemmanuel188@gmail.com>
2025-05-31 13:01:11 +0100

    fixed reseller and admin page

--- app/Livewire/Admin/Hr/Reseller/Index.php
+++ app/Livewire/Admin/Hr/Reseller/Index.php
@@ -10,31 +10,63 @@ use Livewire\WithPagination;
 class Index extends Component
 {
     use WithPagination;
-    
+
     public $perPage = 50;
     public $perPages = [50, 100, 200];
     public $search;
     public $filter;
+    public $param; // For query parameters
+    public $startDate;
+    public $endDate;
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'param' => ['except' => ''],
+        'perPage' => ['except' => 50],
+        'endDate' => ['except' => ''],
+        'startDate' => ['except' => ''],
+        'filter' => ['except' => 'request'],
+    ];
 
     public function mount()
     {
         $this->filter = 'request';
         $this->authorize('view users');
     }
-    
+
     public function render()
-    {        
+    {
+        // Start query builder
         if ($this->filter === 'request') {
             $upgradeRequests = UpgradeRequest::pending()->pluck('user_id')->toArray();
-            $users = User::whereIn('id', $upgradeRequests);
+            $query = User::whereIn('id', $upgradeRequests);
+        } elseif ($this->filter === 'reseller') {
+            $query = User::where('user_level', 'reseller');
+        } else {
+            $query = User::query();
         }
 
-        if ($this->filter === 'reseller') {
-            $users = User::where('user_level', 'reseller');
+        // Apply date filter if set
+        if ($this->startDate && $this->endDate) {
+            $query->whereBetween('created_at', [
+                date('Y-m-d 00:00:00', strtotime($this->startDate)),
+                date('Y-m-d 23:59:59', strtotime($this->endDate))
+            ]);
         }
+
+        // Apply search if set (assuming you have a search scope)
+        if ($this->search) {
+            $query->search($this->search);
+        }
+
+        $resellers = $query
+            ->withTrashed()
+            ->orderBy('account_balance', 'desc')
+            ->latest()
+            ->paginate($this->perPage);
 
         return view('livewire.admin.hr.reseller.index', [
-            'resellers' =>    $users->search($this->search)->latest()->paginate($this->perPage)
+            'resellers' => $resellers
         ]);
     }
 }
