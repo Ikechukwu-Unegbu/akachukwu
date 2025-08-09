064a2022b05684abb2743b4dbcc8201d60a6a604
Ikechukwu-Unegbu <iunegbu94@yahoo.com>
2025-08-09 23:27:36 +0100

    m

---
+++ database/migrations/2025_08_09_231628_create_referral_contests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('referral_contests', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->boolean('active');
+            $table->unsignedBigInteger('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('referral_contests');
+    }
+};
