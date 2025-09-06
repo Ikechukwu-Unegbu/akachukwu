85661a05bf19d2812ceb6ef3e4ddd08b3e32244d
Ikechukwu-Unegbu <iunegbu94@yahoo.com>
2025-09-07 00:58:50 +0100

    M

--- app/Services/Payment/Crypto/QuidaxxService.php
+++ app/Services/Payment/Crypto/QuidaxxService.php
@@ -147,29 +147,7 @@ class QuidaxxService
 
 
 
-    /**
-     * This method when called generates the rate for swapping the $fromCurrency to $toCurrency
-     * **/ 
-    public function generateSwapQuotation($userQuidaxId, $fromCurrency, $froAmount, $toCurrency, $toAmount=null)
-    {
-        $response = $this->makeRequest('get', "users/{$userQuidaxId}/swap_quotation", [
-            'from_currency'=>$fromCurrency,
-            'from_amount'=>$froAmount,
-            'to_currency'=>$toCurrency
-        ]);
-        return $response;
-    }
-
-    /**
-     * Confirm swap
-     * */ 
-    public function confirmQuidaxSwap($quotationId, $userQuidaxId)
-    {
-       
-        $response = $this->makeRequest('POST', "/users/{$userQuidaxId}/swap_quotation/{$quotationId}/confirm");
-        return $response;
-    }
-
+  
     /**
      * Get supported currencies
      */
